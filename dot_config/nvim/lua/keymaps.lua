68ba8f2884ad992bc5a18ab82dfbe48395de9ecd
ama434 <89720399+ama434@users.noreply.github.com>
2025-04-10 11:44:37 +0900

    added settings

---
+++ dot_config/nvim/lua/keymaps.lua
@@ -0,0 +1,5 @@
+-- fetch keymap
+local map = vim.api.nvim_set_keymap
+
+-- update leader key to space
+vim.g.mapleader = ","
---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' }
+  keys = {
+      -- ファイル検索
+      {
+        "<leader>ff",
+        function() require("telescope.builtin").find_files() end,
+        desc = "Telescope: ファイルを検索",
+      },
+      -- ライブ検索
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep() end,
+        desc = "Telescope: ライブ検索",
+      },
+      -- バッファ検索
+      {
+        "<leader>fb",
+        function() require("telescope.builtin").buffers() end,
+        desc = "Telescope: バッファを検索",
+      },
+      -- ヘルプタグ検索
+      {
+        "<leader>fh",
+        function() require("telescope.builtin").help_tags() end,
+        desc = "Telescope: ヘルプタグを検索",
+      },
+      -- カスタム文字列検索
+      {
+        "<leader>fs",
+        function()
+          require("telescope.builtin").grep_string({ search = vim.fn.input("Search > ") })
+        end,
+        desc = "Telescope: カスタム検索文字列で検索",
+      },
+  }
+}
