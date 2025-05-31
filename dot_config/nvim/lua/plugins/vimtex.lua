507fadf61059c03470ed17e27c103a0d22f0f800
ama434 <89720399+ama434@users.noreply.github.com>
2025-05-31 21:53:05 +0900

    add: skim settings

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   -- tag = "v2.15",
   init = function()
-    vim.g.vimtex_view_method = "general"
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_quickfix_open_on_warning = 0
   end
 }
